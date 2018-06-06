420dc5d796c825c8fc361e7b75a09a0948114136
Nathan Graule <solarliner@gmail.com>
2018-06-06 16:29:40 +0200

    Set debug log back to stdout

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from PIL import Image
 
 from terrainlib import reader, terrain
 
-logging.basicConfig(filename='debug.log',format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
 
 
 def main():
-    terrain_gen = terrain.DiamondSquareGenerator(8, 0.1)
+    terrain_gen = terrain.DiamondSquareGenerator(8, 0.3)
     terr = terrain_gen()
     img_reader = reader.PILReader()
     img = img_reader(terr)
     with open('terrain_final.txt', mode='w') as f:
         f.write(repr(terr))
-    img.convert('RGB').save('terrain_final.png')
+    img.convert('L', dither=None).save('terrain_final.png')
 
 if __name__ == '__main__':
     main()
