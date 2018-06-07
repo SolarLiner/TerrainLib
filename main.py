6b9ed58ecfe89470441740533acbf0b30128e2bb
Nathan Graule <solarliner@gmail.com>
2018-06-07 16:15:08 +0200

    Changed dev entrypoint to work on DiamonSquare again

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 
 def main():
-    terr = terrain.ImageGenerator('data/terrain_input.png', terrain.ImageGenerator.BITDEPTH_16)
+    terr = terrain.DiamondSquareGenerator(10, 0.5)
     img_reader = reader.PILReader(reader.PILReader.BITDEPTH_8)
     img = img_reader(terr())
     img.save('terrain_out.png')
