efee51b9fa92eb204ff38549a360630842d1e819
Nathan Graule <solarliner@gmail.com>
2018-06-07 15:57:34 +0200

    Set proper mode on PILReader

--- terrainlib/reader.py
+++ terrainlib/reader.py
@@ -7,7 +7,7 @@ from .terrain import Terrain
 
 class TerrainReader(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __call__(self, terrain, *args, **kwargs):
+    def __call__(self, terrain: Terrain, *args, **kwargs):
         """Caller to process terrain data and output it into another format."""
 
 
@@ -17,11 +17,17 @@ class PILReader(TerrainReader):
     BITDEPTH_16 = 2**16-1
     BITDEPTH_8 = 2**8-1
     
-    def __init__(self, bitdepth):
+    def __init__(self, bitdepth: int):
         if not bitdepth in [self.BITDEPTH_8, self.BITDEPTH_16, self.BITDEPTH_32, self.BITDEPTH_FLOAT]:
             raise TypeError('Bitdepth should be a valid number')
         self.bitdepth = bitdepth
 
-    def __call__(self, terrain):
+    def __call__(self, terrain: Terrain):
         arr = numpy.array(terrain._heightmap, dtype=float)
-        return Image.fromarray(numpy.multiply(arr, self.bitdepth, 'F'))
+        if self.bitdepth == self.BITDEPTH_FLOAT:
+            mode = 'F'
+        elif self.bitdepth == self.BITDEPTH_8:
+            mode = 'L'
+        else:
+            mode = 'I'
+        return Image.fromarray(numpy.multiply(arr, self.bitdepth), mode)
