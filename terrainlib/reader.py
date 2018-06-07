b30031192c1e4eb5cea5ae973ca55aff98bb4d7a
Nathan Graule <solarliner@gmail.com>
2018-06-07 15:36:38 +0200

    Add support for extra bitdepths on PILReader

--- terrainlib/reader.py
+++ terrainlib/reader.py
@@ -12,6 +12,20 @@ class TerrainReader(metaclass=abc.ABCMeta):
 
 
 class PILReader(TerrainReader):
+    BITDEPTH_FLOAT = 0
+    BITDEPTH_32 = 1
+    BITDEPTH_16 = 2
+    BITDEPTH_8 = 3
+    
+    def __init__(self, bitdepth=BITDEPTH_16):
+        self.bitdepth = bitdepth
+
     def __call__(self, terrain):
-        arr = numpy.array(terrain._heightmap, dtype=float)
-        return Image.fromarray(numpy.multiply(arr, 255.0), 'L')
+        arr = numpy.array(terrain._heightmap, dtype=float)        
+        if self.bitdepth == self.BITDEPTH_8:
+            return Image.fromarray(numpy.multiply(arr, 255.0), 'L')
+        if self.bitdepth == self.BITDEPTH_16:
+            return Image.fromarray(numpy.multiply(arr, 2**16-1), 'I')
+        if self.bitdepth == self.BITDEPTH_32:
+            return Image.fromarray(numpy.multiply(arr, 2**32-1), 'I')
+        return Image.fromarray(arr, 'F')
