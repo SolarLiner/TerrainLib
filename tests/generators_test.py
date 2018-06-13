f87f0924a1f5cd840247e2311314f8732f522613
Nathan Graule <solarliner@gmail.com>
2018-06-13 19:57:10 +0200

    Setup testing and coverage report

---
+++ tests/generators_test.py
@@ -0,0 +1,50 @@
+import random
+import unittest
+
+import numpy
+
+from terrainlib.generators.procedural import DiamondSquareGenerator, VoronoiGenerator
+from terrainlib.terrain import Terrain
+
+class TestDiamondSquareGenerator:
+    def test_terrain_size(self):
+        gen = DiamondSquareGenerator(5, 0.1)
+        assert gen().size == 33
+
+    @staticmethod
+    def assert_terrains_equal(terr1, terr2):
+        assert terr1 == terr2
+
+    @staticmethod
+    def assert_terrains_different(terr1, terr2):
+        assert not terr1 == terr2
+
+    def test_same_seed(self):
+        for i in range(5):
+            seed = random.random()
+            gen1 = DiamondSquareGenerator(5, i/10, seed)
+            gen2 = DiamondSquareGenerator(5, i/10, seed)
+
+            yield self.assert_terrains_equal, gen1(), gen2()
+    
+    def test_diff_seeds(self):
+        for i in range(5):
+            seed1 = random.random()
+            seed2 = random.randint(5, 1e80)
+            
+            gen1 = DiamondSquareGenerator(5, i/10, seed1)
+            gen2 = DiamondSquareGenerator(5, i/10, seed2)
+
+            yield self.assert_terrains_different, gen1(), gen2()
+
+    @staticmethod
+    def assert_terrain_minmax(terrain: Terrain):
+        max = numpy.max(terrain._heightmap)
+        min = numpy.min(terrain._heightmap)
+
+        assert max <= 1.0 and min >= 0.0
+
+    def test_terrain_minmax(self):
+        for i in numpy.linspace(0.1, 1., 8).tolist():
+            gen = DiamondSquareGenerator(5, i)
+            yield self.assert_terrain_minmax, gen()
