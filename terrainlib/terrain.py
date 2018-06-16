f49094c9292487d3cfd415ef9bcc35a00ee77241
Nathan Graule <solarliner@gmail.com>
2018-06-17 00:15:13 +0200

    Convert internal array storage to numpy

--- terrainlib/terrain.py
+++ terrainlib/terrain.py
@@ -18,12 +18,13 @@ to function."""
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from itertools import product
 
+import numpy
 
 
 class Terrain:
     def __init__(self, size):
         self._size = size
-        self._heightmap = [[0 for _ in range(size)] for _ in range(size)]
+        self._heightmap = numpy.zeros((size, size))
 
     @property
     def size(self):
@@ -38,52 +39,45 @@ class Terrain:
             frac_part = key[0] - int(key[0])
             cols = frac_part * next_cols + (1.0 - frac_part) * cols
 
+        if isinstance(key[1], int):
+            val = cols[key[1]]
+        if isinstance(key[1], float):
+            val = cols[int(key[1]) % self.size]
+            val_next = cols[int(key[1]+1) % self.size]
+            frac_part = key[1] - int(key[1])
+            val = frac_part * val_next + (1.0 - frac_part) * val
+
+        return val
+
     def __setitem__(self, key, value):
-        self._heightmap[key[1] % self.size][key[0] % self.size] = value
+        self._heightmap[key[1] % self.size,key[0] % self.size] = value
 
     def __eq__(self, other):
         if isinstance(other, Terrain):
-            if not other.size == self.size:
-                return False
-            return all(self[x,y] == other[x,y] for x in range(self.size) for y in range(self.size))
-        return False
+            return numpy.equal(self._heightmap, other._heightmap)
+        else:
+            return numpy.equal(self._heightmap, other)
 
     def __add__(self, other):
-        res = Terrain(self.size)
-        if isinstance(other, (int, float)):
-            for x,y in product(range(self.size), repeat=2):
-                res[x,y] = self[x,y] + other
-            return res
         if isinstance(other, Terrain):
-            for x,y in product(range(self.size), repeat=2):
-                res[x,y] = self[x,y] + other[x,y]
-            return res
-        raise ArithmeticError("Type %s cannot add to type Terrain" % type(other).__name__)
+            return numpy.add(self._heightmap, other._heightmap)
+        else:
+            return numpy.add(self._heightmap, other)
 
     def __sub__(self, other):
-        res = Terrain(self.size)
-        if isinstance(other, (int, float)):
-            return self + (-other)
         if isinstance(other, Terrain):
-            for x,y in product(range(self.size), repeat=2):
-                res[x,y] = self[x,y] - other[x,y]
-            return res
-        raise ArithmeticError("Type %s cannot subtract to type Terrain" % type(other).__name__)
+            return numpy.subtract(self._heightmap, other._heightmap)
+        else:
+            return numpy.subtract(self._heightmap, other)
 
     def __mul__(self, other):
-        res = Terrain(self.size)
-        if isinstance(other, (int, float, bool)):
-            for x,y in product(range(self.size), repeat=2):
-                res[x,y] = self[x,y] * other
-            return res
         if isinstance(other, Terrain):
-            for x,y in product(range(self.size)):
-                res[x,y] = self[x,y] * other[x,y]
-            return res
-        raise ArithmeticError("Type %s cannot multiply to type Terrain" % type(other).__name__)
+            return numpy.multiply(self._heightmap, other._heightmap)
+        else:
+            return numpy.multiply(self._heightmap, other)
 
     def __str__(self):
-        return "Terrain object of size {}".format(self.size)
+        return "Terrain(size={}): {}".format(self.size, str(self._heightmap))
 
     def __repr__(self):
-        return '\n'.join([str(self._heightmap[i]) for i in range(self.size)])
+        return repr(self._heightmap)
