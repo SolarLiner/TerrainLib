38ef7a38b86f08e7c6a666bb8cf0d736b5805033
Nathan Graule <solarliner@gmail.com>
2018-07-05 18:13:52 +0200

    Break down image save test into single tests for each bit depth - gives #2 a reliable test case to build onto

--- tests/readers_test.py
+++ tests/readers_test.py
@@ -1,4 +1,4 @@
-import math
+from pathlib import Path
 
 from terrainlib.generators.procedural import DiamondSquareGenerator
 from terrainlib.readers.image import PILImageReader
@@ -7,9 +7,26 @@ from terrainlib.readers.image import PILImageReader
 class TestImageExport:
     def setup(self):
         self.terrain = DiamondSquareGenerator(6, .1)()
+        self.file_png = Path('test_terrain_export.png')
+        self.file_tif = Path('test_terrain_export.tif')
 
-    def test_save_image(self):
-        for bitdepth in [PILImageReader.BITDEPTH_8, PILImageReader.BITDEPTH_16, PILImageReader.BITDEPTH_32, PILImageReader.BITDEPTH_FLOAT]:
-            reader = PILImageReader(bitdepth)
-            img = reader(self.terrain)
-            img.save('terrain_test.tif')
+    def save_bitdepth_factory(self, bitdepth, filepath):
+        img_reader = PILImageReader(bitdepth)
+        img = img_reader(self.terrain)
+        img.save(filepath)
+
+    def test_save_8bit_png(self):
+        self.save_bitdepth_factory(PILImageReader.BITDEPTH_8, self.file_png.resolve())
+        assert self.file_png.is_file(), '8 bit PNG file exists'
+
+    def test_save_16bit_png(self):
+        self.save_bitdepth_factory(PILImageReader.BITDEPTH_16, self.file_png.resolve())
+        assert self.file_png.is_fifo(), '16 bit PNG file exists'
+
+    def test_save_32bit_tif(self):
+        self.save_bitdepth_factory(PILImageReader.BITDEPTH_32, self.file_tif.resolve())
+        assert self.file_tif.is_file(), '32 bit TIFF file exists'
+
+    def test_save_float_tif(self):
+        self.save_bitdepth_factory(PILImageReader.BITDEPTH_FLOAT, self.file_tif.resolve())
+        assert self.file_tif.is_file(), 'Float TIFF file exists'
