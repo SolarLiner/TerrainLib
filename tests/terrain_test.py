f25f992135a9be149e9b8c5b80503dbefc463d67
Nathan Graule <solarliner@gmail.com>
2018-07-05 13:48:47 +0200

    Reformat terrain test file

--- tests/terrain_test.py
+++ tests/terrain_test.py
@@ -11,22 +11,22 @@ class TestTerrain:
         assert terr.size == 256
 
     def test_accepts_array(self):
-        terr = Terrain(numpy.random.uniform(size=(256,256)))
+        terr = Terrain(numpy.random.uniform(size=(256, 256)))
 
         assert terr.size == 256
 
     @raises(TypeError)
     def test_throws_on_nonsquare_array(self):
-        terr = Terrain(array=numpy.random.uniform(size=(256,128)))
+        terr = Terrain(array=numpy.random.uniform(size=(256, 128)))
 
     @raises(TypeError)
     def test_throws_on_non_2d_array(self):
         terr = Terrain(array=numpy.random.uniform(size=128))
 
     def test_terrain_addition(self):
-        arr1 = numpy.ones((128,128)) * 0.1
-        arr2 = numpy.ones((128,128)) * 0.2
-        res = numpy.ones((128,128)) * 0.3
+        arr1 = numpy.ones((128, 128)) * 0.1
+        arr2 = numpy.ones((128, 128)) * 0.2
+        res = numpy.ones((128, 128)) * 0.3
 
         terr1 = Terrain(array=arr1)
         terr2 = Terrain(array=arr2)
@@ -34,9 +34,9 @@ class TestTerrain:
         assert (terr1 + terr2)._heightmap == res
 
     def test_terrain_subtraction(self):
-        arr1 = numpy.ones((128,128)) * 0.3
-        arr2 = numpy.ones((128,128)) * 0.2
-        res = numpy.ones((128,128)) * 0.1
+        arr1 = numpy.ones((128, 128)) * 0.3
+        arr2 = numpy.ones((128, 128)) * 0.2
+        res = numpy.ones((128, 128)) * 0.1
 
         terr1 = Terrain(array=arr1)
         terr2 = Terrain(array=arr2)
@@ -44,9 +44,9 @@ class TestTerrain:
         assert (terr1 - terr2)._heightmap == res
 
     def test_terrain_multiplication(self):
-        arr1 = numpy.ones((128,128)) * 2.0
-        arr2 = numpy.ones((128,128)) * .5
-        res = numpy.ones((128,128))
+        arr1 = numpy.ones((128, 128)) * 2.0
+        arr2 = numpy.ones((128, 128)) * .5
+        res = numpy.ones((128, 128))
 
         terr1 = Terrain(array=arr1)
         terr2 = Terrain(array=arr2)
@@ -54,9 +54,9 @@ class TestTerrain:
         assert (terr1 * terr2)._heightmap == res
 
     def test_terrain_division(self):
-        arr1 = numpy.ones((128,128)) * 2.0
-        arr2 = numpy.ones((128,128)) * .5
-        res = numpy.ones((128,128))
+        arr1 = numpy.ones((128, 128)) * 2.0
+        arr2 = numpy.ones((128, 128)) * .5
+        res = numpy.ones((128, 128))
 
         terr1 = Terrain(array=arr1)
         terr2 = Terrain(array=arr2)
